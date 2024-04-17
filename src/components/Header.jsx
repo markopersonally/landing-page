28aed41de002add355cdaacbc298775df941ab3d
mkpage1337 <mrku1337@gmail.com>
2024-04-17 19:53:45 +0200

    codding

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+const Header = () => {
+    return <header className="relative m-auto max-w-[1440px] h-screen">
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 h-screen w-screen bg-no-repeat bg-black bg-zinc-400"></div>
+        <h1 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-6xl text-black">markopersonally</h1>
+    </header>
+}
+
+export default Header;
